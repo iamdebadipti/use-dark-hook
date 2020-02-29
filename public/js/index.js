b6514779ce577a1f848b557f429a75d8c5148571
iamdebadipti <iamdebadipti@gmail.com>
2020-02-29 17:43:27 +0530

    added markup and style, used custom hook

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,17 @@ import '../css/main.css';
 // components
 import MainContainer from './MainContainer';
 
+// use-dark-hook
+import { useDarkHook } from '../../src/index';
+
 const App = () => {
+  const [mode, toggleMode] = useDarkHook('dark');
+
   return (
     <MainContainer>
-      <h1>hello</h1>
+      <h1>Hello "use-dark-hook"</h1>
+      <h2>Current Mode: {mode}</h2>
+      <button onClick={() => toggleMode()}>toggle mode</button>
     </MainContainer>
   );
 };
