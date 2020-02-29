2fecce3cdc09c1f68d7c3e4579986ebfb75cb950
iamdebadipti <iamdebadipti@gmail.com>
2020-02-29 17:42:16 +0530

    added storage manipulation and get preferred theme helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,34 @@
-export const getLocalStorageState = () => {
-  console.log('GET LocalStorageState');
+// perform async localstorage operations
+export const asyncLocalStorage = {
+  setItem: function(key, value) {
+    return Promise.resolve().then(function() {
+      localStorage.setItem(key, value);
+    });
+  },
+  getItem: function(key) {
+    return Promise.resolve().then(function() {
+      return localStorage.getItem(key);
+    });
+  },
 };
 
-export const setLocalStorageState = () => {
-  console.log('SET LocalStorageState');
-};
+// get preferred system theme of the user. returned value: String: "dark", "light", "default"
+export const getPreferredSystemTheme = () => {
+  let preferredTheme = null;
 
-// export const getLocalStorageState = () => {
-//   let mql = window.matchMedia('(max-width: 600px)');
-// };
+  const colorSchemes = {
+    dark: '(prefers-color-scheme: dark)',
+    light: '(prefers-color-scheme: light)',
+    default: '(prefers-color-scheme: no-preference)',
+  };
+
+  if (window.matchMedia) {
+    Object.keys(colorSchemes).forEach(schemeName => {
+      if (window.matchMedia(colorSchemes[schemeName]).matches) {
+        preferredTheme = schemeName;
+      }
+    });
+  }
+
+  return preferredTheme;
+};
