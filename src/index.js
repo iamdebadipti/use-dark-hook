adf444fed0cdacec6058df0f3b237b0d09cb82b2
iamdebadipti <iamdebadipti@gmail.com>
2020-03-03 09:25:30 +0530

    working version

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 import { useEffect, useState } from 'react';
 
-import { getPreferredSystemTheme, asyncLocalStorage } from './helpers';
+import { getPreferredSystemTheme, asyncLocalStorage, setBodyClassName } from './helpers';
 
 export function useDarkHook(defaultMode) {
   const [mode, setMode] = useState();
 
   useEffect(() => {
-    asyncLocalStorage.getItem('user_theme').then(res => {
-      if (res !== null) {
-        return setMode(res);
-      } else {
-        if (defaultMode) {
-          asyncLocalStorage.setItem('user_theme', defaultMode).then(() => {
-            return setMode(defaultMode);
-          });
+    asyncLocalStorage
+      .getItem('user_theme')
+      .then(res => {
+        if (res !== null) {
+          return setMode(res);
         } else {
-          let systemTheme = getPreferredSystemTheme();
-          if (systemTheme === 'dark' || systemTheme === 'light') {
-            return setMode(systemTheme);
+          if (defaultMode) {
+            asyncLocalStorage.setItem('user_theme', defaultMode).then(() => {
+              return setMode(defaultMode);
+            });
+          } else {
+            let systemTheme = getPreferredSystemTheme();
+            if (systemTheme === 'dark' || systemTheme === 'light') {
+              return setMode(systemTheme);
+            }
           }
         }
-      }
-    });
-  }, []);
+      })
+      .then(() => {
+        setBodyClassName(mode);
+      });
+  }, [mode]);
 
   const toggleMode = () => {
-    console.log('toggling...');
+    let modeToggled = mode === 'dark' ? 'light' : 'dark';
+    asyncLocalStorage.setItem('user_theme', modeToggled).then(() => {
+      setMode(modeToggled);
+      setBodyClassName(modeToggled);
+    });
   };
 
   return [mode, toggleMode];
